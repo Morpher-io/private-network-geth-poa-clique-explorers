8c7c5088de9e226f06199f349730e8c25f32d4c5
Thomas Wiesner <{ID}+{username}@users.noreply.github.com>
2022-09-30 10:13:28 +0200

    added variable network id and fixed init for newer geth

--- config/client-start.sh
+++ config/client-start.sh
@@ -4,7 +4,8 @@ DIR="/app/datadir/geth"
 if [ ! -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo "Installing config files in ${DIR}..."
-  geth init /app/genesis.json --datadir=/app/datadir
+  sed -i 's/"chainId": 21000/"chainId": '$NETWORK_CHAIN_ID'/' /app/genesis.json
+  geth init --datadir=/app/datadir /app/genesis.json 
 
   
   echo "[\"enode://${ENODEPUB_1}@signer-1:30303\",\"enode://${ENODEPUB_2}@signer-2:30303\"]" > /app/datadir/static-nodes.json
@@ -13,5 +14,5 @@ sleep 3;
 
 ipaddress=$(hostname -i)
 
-geth --bootnodes "enode://${ENODEPUB_1}@signer-1:30303,enode://${ENODEPUB_2}@signer-2:30303" --networkid="210" --datadir=/app/datadir --http --http.addr="0.0.0.0" --http.port="8545" --http.corsdomain="*" --http.vhosts="*" --http.api="eth,net,web3,debug,txpool,trace" --ws  --ws.origins="*" --ws.addr="0.0.0.0" --ws.port="8546" --syncmode "full" --gcmode archive --nat extip:${ipaddress}
+geth --bootnodes "enode://${ENODEPUB_1}@signer-1:30303,enode://${ENODEPUB_2}@signer-2:30303" --networkid="${NETWORK_CHAIN_ID}" --datadir=/app/datadir --http --http.addr="0.0.0.0" --http.port="8545" --http.corsdomain="*" --http.vhosts="*" --http.api="eth,net,web3,debug,txpool,trace" --ws  --ws.origins="*" --ws.addr="0.0.0.0" --ws.port="8546" --syncmode "full" --gcmode archive --nat extip:${ipaddress} --identity "Morpher Dev Demo Network"
 
--- config/miner-start.sh
+++ config/miner-start.sh
@@ -4,7 +4,8 @@ DIR="/app/datadir/geth"
 if [ ! -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo "Installing config files in ${DIR}..."
-  geth init /app/genesis.json --datadir=/app/datadir
+  sed -i 's/"chainId": 21000/"chainId": '$NETWORK_CHAIN_ID'/' /app/genesis.json
+  geth init --datadir=/app/datadir /app/genesis.json 
 
   echo ${SIGNERKEY} > /app/key
   geth account import --datadir=/app/datadir --password <(echo $SIGNERPW) /app/key
@@ -16,4 +17,4 @@ sleep 3;
 
 ipaddress=$(hostname -i)
 
-geth --bootnodes "enode://${ENODEPUB_1}@signer-1:30303,enode://${ENODEPUB_2}@signer-2:30303" --verbosity 8 --gcmode archive  --syncmode "full" --networkid="210" --datadir=/app/datadir --http --http.addr="0.0.0.0" --http.port="8545" --http.corsdomain="*" --http.vhosts="*" --http.api="eth,net,web3" --allow-insecure-unlock --miner.etherbase=$SIGNERADD --mine --miner.gasprice 1 --unlock $SIGNERADD --password <(echo $SIGNERPW) -nodekey <(echo ${SIGNERKEY:?NOT SET})  --nat extip:${ipaddress}
+geth --bootnodes "enode://${ENODEPUB_1}@signer-1:30303,enode://${ENODEPUB_2}@signer-2:30303" --verbosity 3 --gcmode archive  --syncmode "full" --networkid="${NETWORK_CHAIN_ID}" --datadir=/app/datadir --http --http.addr="0.0.0.0" --http.port="8545" --http.corsdomain="*" --http.vhosts="*" --http.api="eth,net,web3" --allow-insecure-unlock --miner.etherbase=$SIGNERADD --mine --miner.gasprice 1 --unlock $SIGNERADD --password <(echo $SIGNERPW) -nodekey <(echo ${SIGNERKEY:?NOT SET})  --nat extip:${ipaddress} --identity "Morpher Dev Demo Network"
