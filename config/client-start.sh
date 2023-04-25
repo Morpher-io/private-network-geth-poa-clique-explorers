5a8a8f50f15df8b19f4c34c41da4aa2a23e5ddf8
Thomas Wiesner (thomaswiesner.com) <485781+tomw1808@users.noreply.github.com>
2023-04-25 08:51:25 +0200

    updated Dockerfile for running with new config.toml

--- config/client-start.sh
+++ config/client-start.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
 DIR="/app/datadir/geth"
-if [ ! -d "$DIR" ]; then
+# if [ ! -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo "Installing config files in ${DIR}..."
   sed -i 's/"chainId": 21000/"chainId": '$NETWORK_CHAIN_ID'/' /app/genesis.json
   geth init --datadir=/app/datadir /app/genesis.json 
-
+  sed -i 's|StaticNodes = \[\]|StaticNodes = ["enode://'$ENODEPUB_1'@'$SIGNER1DOMAIN':30303","enode://'$ENODEPUB_2'@'$SIGNER2DOMAIN':30303"]|' /app/config.toml
+  sed -i 's|BootstrapNodes = \[\]|BootstrapNodes = ["enode://'$ENODEPUB_1'@'$SIGNER1DOMAIN':30303","enode://'$ENODEPUB_2'@'$SIGNER2DOMAIN':30303"]|' /app/config.toml
+  sed -i 's|BootstrapNodesV5 = \[\]|BootstrapNodesV5 = ["enode://'$ENODEPUB_1'@'$SIGNER1DOMAIN':30303","enode://'$ENODEPUB_2'@'$SIGNER2DOMAIN':30303"]|' /app/config.toml
+  sed -i 's|SyncMode = "full"|SyncMode = "light"|' /app/config.toml
+  sed -i 's|NoPruning = true|NoPruning = false|' /app/config.toml
+  sed -i 's/NetworkId = 210/NetworkId = '$NETWORK_CHAIN_ID'/' /app/config.toml
   
-  echo "[\"enode://${ENODEPUB_1}@signer-1:30303\",\"enode://${ENODEPUB_2}@signer-2:30303\"]" > /app/datadir/static-nodes.json
-fi
+  # echo "[\"enode://${ENODEPUB_1}@signer-1:30303\",\"enode://${ENODEPUB_2}@signer-2:30303\"]" > /app/datadir/static-nodes.json
+# fi
 sleep 3;
 
 ipaddress=$(hostname -i)
 
-geth --bootnodes "enode://${ENODEPUB_1}@signer-1:30303,enode://${ENODEPUB_2}@signer-2:30303" --networkid="${NETWORK_CHAIN_ID}" --datadir=/app/datadir --http --http.addr="0.0.0.0" --http.port="8545" --http.corsdomain="*" --http.vhosts="*" --http.api="eth,net,web3,debug,txpool,trace" --ws  --ws.origins="*" --ws.addr="0.0.0.0" --ws.port="8546" --syncmode "full" --gcmode archive --nat extip:${ipaddress} --identity "Morpher Dev Demo Network"
+geth --config /app/config.toml --identity "Morpher Dev Demo Network"
 
--- config/miner-start.sh
+++ config/miner-start.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-
+sleep 30 #cloudmap needs that to register the domain
 DIR="/app/datadir/geth"
 # if [ ! -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo "Installing config files in ${DIR}..."
   sed -i 's/"chainId": 21000/"chainId": '$NETWORK_CHAIN_ID'/' /app/genesis.json
   # sed -i 's/"extraData": "0x00000000000000000000000000000000000000000000000000000000000000003c1ed3433916f130a7badf8b1ad770cfa2fd03bed96c84c682a298d964f517a61617a8be7cc67f04/"extraData": "0x0000000000000000000000000000000000000000000000000000000000000000'${SIGNERADD/"0x"/""}${SIGNER2ADD/"0x"/""}'/' /app/genesis.json
-  sed -i 's|StaticNodes = \[\]|StaticNodes = ["enode://'$ENODEPUB_1'@signer-1:30303","enode://'$ENODEPUB_2'@signer-2:30303"]|' /app/config.toml
-  sed -i 's|BootstrapNodes = \[\]|BootstrapNodes = ["enode://'$ENODEPUB_1'@signer-1:30303","enode://'$ENODEPUB_2'@signer-2:30303"]|' /app/config.toml
-  sed -i 's|BootstrapNodesV5 = \[\]|BootstrapNodesV5 = ["enode://'$ENODEPUB_1'@signer-1:30303","enode://'$ENODEPUB_2'@signer-2:30303"]|' /app/config.toml
+  sed -i 's|StaticNodes = \[\]|StaticNodes = ["enode://'$ENODEPUB_1'@'$SIGNER1DOMAIN':30303","enode://'$ENODEPUB_2'@'$SIGNER2DOMAIN':30303"]|' /app/config.toml
+  sed -i 's|BootstrapNodes = \[\]|BootstrapNodes = ["enode://'$ENODEPUB_1'@'$SIGNER1DOMAIN':30303","enode://'$ENODEPUB_2'@'$SIGNER2DOMAIN':30303"]|' /app/config.toml
+  sed -i 's|BootstrapNodesV5 = \[\]|BootstrapNodesV5 = ["enode://'$ENODEPUB_1'@'$SIGNER1DOMAIN':30303","enode://'$ENODEPUB_2'@'$SIGNER2DOMAIN':30303"]|' /app/config.toml
   sed -i 's/NetworkId = 210/NetworkId = '$NETWORK_CHAIN_ID'/' /app/config.toml
   geth init --datadir=/app/datadir /app/genesis.json 
 
@@ -25,4 +25,4 @@ ipaddress=$(hostname -i)
 # geth --bootnodes "enode://${ENODEPUB_1}@signer-1:30303,enode://${ENODEPUB_2}@signer-2:30303" --verbosity 3 --gcmode archive  --syncmode "full" --networkid="${NETWORK_CHAIN_ID}" --datadir=/app/datadir --http --http.addr="0.0.0.0" --http.port="8545" --http.corsdomain="*" --http.vhosts="*" --http.api="eth,net,web3" --allow-insecure-unlock dumpconfig > config.toml
 
 # geth --bootnodes "enode://${ENODEPUB_1}@signer-1:30303,enode://${ENODEPUB_2}@signer-2:30303" --verbosity 3 --gcmode archive  --syncmode "full" --networkid="${NETWORK_CHAIN_ID}" --datadir=/app/datadir --http --http.addr="0.0.0.0" --http.port="8545" --http.corsdomain="*" --http.vhosts="*" --http.api="eth,net,web3" --allow-insecure-unlock --miner.etherbase=$SIGNERADD --mine --miner.gasprice 1 --unlock $SIGNERADD --password <(echo $SIGNERPW) -nodekey <(echo ${SIGNERKEY:?NOT SET})  --identity "Morpher Dev Demo Network"
-geth --config /app/config.toml --verbosity 3 --miner.etherbase=$SIGNERADD --mine --miner.gasprice 1 --unlock $SIGNERADD --password <(echo $SIGNERPW) -nodekey <(echo ${SIGNERKEY:?NOT SET})  --identity "Morpher Dev Demo Network"
+geth --config /app/config.toml --verbosity 3 --light.serve 50 --txlookuplimit 0 --miner.etherbase=$SIGNERADD --mine --miner.gasprice 1 --unlock $SIGNERADD --password <(echo $SIGNERPW) -nodekey <(echo ${SIGNERKEY:?NOT SET})  --identity "Morpher Dev Demo Network"
